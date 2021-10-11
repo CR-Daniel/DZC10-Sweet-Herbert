69c10e009da59119e73a74cb875e00cacf408df3
Alwin_Verbeeten <a.l.verbeeten@student.tue.nl>
2021-10-11 15:48:44 +0200

    blabla

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private AudioClip[] clipsHurt;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step() {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip() {
+        clips = new AudioClip[] {
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_01"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_02"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_03"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_04"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_05"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_06"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_07"),
+            (AudioClip)Resources.Load("Footsteps/Footsteps_Tile_Walk_08")
+        };
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+
+    private void Hurt() {
+        AudioClip hurt = GetRandomClipHurt();
+        audioSource.PlayOneShot(hurt);
+    }
+
+    private AudioClip GetRandomClipHurt() {
+        clipsHurt = new AudioClip[] {
+            (AudioClip)Resources.Load("Screams/Mario1"),
+            (AudioClip)Resources.Load("Screams/Mario2"),
+            (AudioClip)Resources.Load("Screams/Oof"),
+            (AudioClip)Resources.Load("Screams/Wilhelm")
+        };
+        return clipsHurt[UnityEngine.Random.Range(0, clipsHurt.Length)];
+    }
+
+    // private void Hey() {
+    //     AudioClip hey = GetRandomClipHey();
+    //     audioSource.PlayOneShot(hey);
+    // }
+
+    // private AudioClip GetRandomClipHey() {
+    //     clipsHey = new AudioClip[] {
+    //         (AudioClip)Resources.Load("Hey/"),
+    //         (AudioClip)Resources.Load("Hey/"),
+    //         (AudioClip)Resources.Load("Hey/"),
+    //         (AudioClip)Resources.Load("Hey/")
+    //     };
+    //     return clipsHey[UnityEngine.Random.Range(0, clipsHey.Length)];
+    // }
+}
